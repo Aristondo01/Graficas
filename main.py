d125c763c2cae870ed8e5b95f1f159c5370eb8ef
Aristondo01 <ari20880@uvg.edu.gt>
2022-09-18 21:25:34 -0600

    Los 5 modelos

--- main.py
+++ main.py
@@ -10,7 +10,20 @@ camara="medium"
 
 glFondo("jungla")
 
-'''  '''
+
+#Quinto modelo
+objeto="Egg"
+glLookAt((0,0,1), (0,0,0), (0,1,0))
+glViewPort(0,0,115,115)
+glTexture(objeto)
+translate_factor = (0.75,-0.45,0)
+scale_factor = (0.03,0.03,0.03)
+rotate = (0,0,-pi/2)
+glIntensidadLuz(1.05)
+glLoadMMatriz(translate_factor,scale_factor,rotate)
+obj3D(objeto,color)
+
+
 #Primer modelo
 objeto="Falcon"
 glLookAt((0,0,1), (0,0,0), (0,1,0))
@@ -19,7 +32,7 @@ glTexture(objeto)
 translate_factor = (1.3,1.4,0)
 scale_factor = (0.45,0.45,0.45)
 rotate = (0,-16*pi/18,0)
-glIntensidadLuz(2.75)
+glIntensidadLuz(2.95)
 glLoadMMatriz(translate_factor,scale_factor,rotate)
 obj3D(objeto,color)
 
@@ -33,7 +46,7 @@ glTexture(objeto)
 translate_factor = (0.2,-0.7,0)
 scale_factor = (0.15,0.15,0.15)
 rotate = (pi/2,0,0)
-glIntensidadLuz(3.75)
+glIntensidadLuz(3.95)
 glLoadMMatriz(translate_factor,scale_factor,rotate)
 obj3D(objeto,color)
 
@@ -46,7 +59,20 @@ glTexture(objeto)
 translate_factor = (0,-0.97,0)
 scale_factor = (0.03,0.03,0.03)
 rotate = (0,pi/5,0)
-glIntensidadLuz(2)
+glIntensidadLuz(2.35)
+glLoadMMatriz(translate_factor,scale_factor,rotate)
+obj3D(objeto,color)
+
+
+#Cuarto modelo
+objeto="arbol"
+glLookAt((0,0,1), (0,0,0), (0,1,0))
+glViewPort(0,0,150,150)
+glTexture(objeto)
+translate_factor = (1.8,-1,0)
+scale_factor = (0.015,0.015,0.015)
+rotate = (0,0,0)
+glIntensidadLuz(1.75)
 glLoadMMatriz(translate_factor,scale_factor,rotate)
 obj3D(objeto,color)
 
